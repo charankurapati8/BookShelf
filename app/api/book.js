ccf4f5610780f2cf2f5640dc6310d6b1ad44251e
charanreddy <kurapati.reddy@magicedtech.com>
2024-06-12 10:42:58 +0530

    Deleted
    deleted the api/book file and search result page

--- app/api/book.js
+++
@@ -1,9 +0,0 @@
-import { searchBooks } from "@/lib/books";
- export default function handler(req, res) {
-  const { keyword } = req.query;
-  if (!keyword) {
-    return res.status(400).json({ error: 'Keyword is required' });
-  }
-const results = searchBooks(keyword);
-res.status(200).json(results);
-}
