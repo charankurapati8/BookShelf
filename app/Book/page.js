8c4d144e54688bf906bae6c48c5eae0316e404f2
charanreddy <kurapati.reddy@magicedtech.com>
2024-06-19 00:09:42 +0530

    install dependencies for react-icons

--- app/Book/page.js
+++ app/Book/page.js
@@ -4,12 +4,10 @@ import classes from './page.module.css'
 import {  getBooks } from '@/lib/books'
 import { Suspense } from 'react';
 import Link from 'next/link';
-import SearchPage from '../searchpage/searchpage';
  async function Books(){
         const books = await getBooks();
         return <Booksgrid books={books}/>
 }
-
 export default function Book(){
   return(
         <>
@@ -17,13 +15,6 @@ export default function Book(){
            <h1>
             Interesting Books in the browser
             </h1> 
-            <p>choose your favourite book</p>
-            <p className={classes.cta}>
-                <Link href="/Sharebook">share your favourite book</Link>        
-            </p>
-           <SearchPage/>
-            <div> 
-            </div>
         </header>
         <main className={classes.main}>
                 <Suspense fallback={<p className={classes.loading}>fetching books.....</p>}>
