4f44a4cfd9db7bbcda869ff4b84e69c8ea571ba2
charanreddy <kurapati.reddy@magicedtech.com>
2024-06-19 00:05:55 +0530

    created a hamburger menu option
    For browser books,add books,favourite books and remove books

--- component/main-header/main-header.js
+++ component/main-header/main-header.js
@@ -1,9 +1,17 @@
+'use client';
 import React from "react";
+import { useState } from "react";
+import { FaBars } from 'react-icons/fa'
 import Link from "next/link";
 import logoimga from '@/assets/logoimg.png'
 import classes from './main-header.module.css'
 import MainHeaderBackground from "./main-header-background";
 export default function Mainheader(){
+    const [isMenuOpen,setIsMenuOpen] = useState(false);
+    const toggleMenu = () =>{
+        setIsMenuOpen(!isMenuOpen)
+    }
+
     return(
 <>  <header className={classes.header}>
     <MainHeaderBackground/>
@@ -12,13 +20,31 @@ export default function Mainheader(){
                 <img  src={logoimga.src} alt="library"/>
     </Link>
  <h1>List of books in the library</h1>
-            <nav className={classes.nav}>
-                <ul>
-                    <li>
-                        <Link href={"/Book"}>Browse books</Link>
-                    </li>
-                </ul>
-            </nav>
+           <div className={classes.hamburger} onClick={toggleMenu}>
+            <FaBars size={24}/>
+           </div>
+           {
+            isMenuOpen && (
+                <div className={classes.sideMenu}>
+                    <button onClick={toggleMenu} className={classes.closeButton}>x</button>
+                    <ul>
+                        <li>
+                            <Link href={"/Book"}>Browse Books</Link>
+                        </li>
+                        <li>
+                            <Link href={"/Sharebook"}>Add Books</Link>
+                        </li>
+                        <li>
+                            <Link href={"/favouritebooks"}>Favourite Books</Link>
+                        </li>
+                        <li>
+                            <Link href={"/removebooks"}>Remove books</Link>
+                        </li>
+                    </ul>
+
+                </div>
+            )
+           }
         </header>
         </>
     
