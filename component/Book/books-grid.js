b8cfc4e766a653af77f9aff7142e7ce52fbbbb8e
charanreddy <kurapati.reddy@magicedtech.com>
2024-06-28 12:44:37 +0530

    created useeffect to fetch fav book from the api

--- component/Book/books-grid.js
+++ component/Book/books-grid.js
@@ -1,13 +1,34 @@
+'use client';
 import classes from './books-grid.module.css'
 import BookItem from './book-item'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 export default function Booksgrid({books}){
+    const [favourite,setFavourite] = useState([]);
+    useEffect(()=>{
+        const fetchfavbooks = async () =>{
+            const response = await fetch('/api/favourite');
+            if(response.ok){
+                const data = await response.json();
+                setFavourite(data);
+            }
+        }
+        fetchfavbooks();
+    },[]);
     return(
         <ul className={classes.books}>
-            {books.map((book)=> <li key={book.id}>
-            <BookItem {...book}/>
-            </li>)}
+            {books.map((book)=> 
+               {
+               const isFavourite  = favourite.some((favbook)=>favbook.slug===book.slug);
+            return(
+                    <li key={book.id}>
+                    <BookItem
+                   initialFavourite={isFavourite}
+                   slug={book.slug}
+                    {...book}/> 
+                    </li>
+           );
+})}
         </ul>
-    )
+    );
 
 }
